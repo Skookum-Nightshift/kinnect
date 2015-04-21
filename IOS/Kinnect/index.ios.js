fd8b41c6df0081e65cfba2a3abd44f9b3c774d0e
aliv1 <aliving631@gmail.com>
2015-04-21 12:39:37 -0400

    Loading & Login Pages
    First iteration. Loading page needs loading and resolution tweaks.
    Working on the gradient for background and linking the Kinnect icon.

--- IOS/Kinnect/index.ios.js
+++ IOS/Kinnect/index.ios.js
@@ -6,10 +6,11 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
-  TouchableHighlight
+  TouchableHighlight,
 } = React;
 
 var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
@@ -17,10 +18,9 @@ var FacebookLoginManager = require('NativeModules').FacebookLoginManager;
 var Kinnect = React.createClass({
   getInitialState() {
     return {
-      result: '...'
+      result: 'Thanks for using Kinnect to share memories with your loved ones. Log into Facebook to get started'
     }
   },
-
   login() {
     FacebookLoginManager.newSession((error, info) => {
       if (error) {
@@ -31,37 +31,83 @@ var Kinnect = React.createClass({
       }
     });
   },
-
   render: function() {
     return (
       <View style={styles.container}>
-        <TouchableHighlight onPress={this.login} >
-          <Text>Login</Text>
-        </TouchableHighlight>
-        <Text style={styles.welcome}>
+        <View style={styles.box}>
+        <Image
+          source={{uri:'/Users/create/Desktop/Skookum_Nightshift/kinnect_git/IOS/Kinnect/iOS/Images.xcassets/kinnect.svg'}}
+          style={styles.image}/>
+          <Text style={styles.kinnect}>
+          KINNECT
+          </Text>
+          <Text style={styles.welcome}>
           {JSON.stringify(this.state.result)}
         </Text>
+        <TouchableHighlight onPress={this.login} style={styles.login} >
+          <Text style={styles.text}>Login with Facebook</Text>
+        </TouchableHighlight>
+        </View>
       </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
+  image:{
+    borderWidth: 0.25,
+    borderColor:'#FFFFFF',
+    position: 'relative',
+    width: 40,
+    height: 40,
+    alignSelf:'center',
+    opacity: 100,
+  },
   container: {
+    borderWidth: 0.25,
+    borderColor: '#FFFFFF',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#000FFF',
+  },
+  box:{
+    borderBottomWidth: 0.25,
+    borderTopWidth: 0.25,
+    borderColor: '#FFFFFF',
+    height: 300,
+    justifyContent: 'center',
+    padding: 25,
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 10,
     textAlign: 'center',
     margin: 10,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  kinnect: {
+    fontFamily: 'Futura',
+    textAlign: 'center',
+    fontSize: 10,
+    color: '#FFFFFF',
   },
   instructions: {
     textAlign: 'center',
-    color: '#333333',
+    color: '#FFFFFF',
     marginBottom: 5,
+  },
+  login:{
+    borderWidth: 0.25,
+    borderColor:'#FFFFFF',
+    alignSelf: 'center',
+    height: 50,
+    width: 100,
+    padding: 5,
+  },
+  text:{
+    textAlign:'center',
+    color:'#000000',
   },
 });
 
